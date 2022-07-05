b493344250f2245cd062f3c63ad0573e9c5dc968
Anton Tolkachev <antontolkachev@gmail.com>
2022-07-05 11:15:44 +0300

    update DTO

--- packages/api/src/request-workflow/dto/request.dto.ts
+++ packages/api/src/request-workflow/dto/request.dto.ts
@@ -2,8 +2,12 @@ import { IVacationRequest } from '@kedo-team/data-model'
 
 
 export class RequestDTO implements IVacationRequest {
+    id: string;
+    status: string;
+    createdAt: string;
+    comment: string;
     ownerUserId: string
     requestTypeName: 'VACATION'
     payload: { dateFrom: string; dateTo: string; isPayed: boolean }
-   
+
 }
